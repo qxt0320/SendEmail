3239a3ab4330d6f8548629a2e3528f9032cca517
yyc <1185553559@qq.com>
2023-11-04 21:13:49 +0800

    添加gui

--- email_content_generator.py
+++ email_content_generator.py
@@ -1,8 +1,10 @@
+import re
 def create_grades_entry(df, student_id):
     # 根据学生ID筛选出该生的所有成绩
     student_grades = df[df['学号'] == student_id]
-    # 创建成绩条目的列表
-    grades_entry = []
+
+    # 创建成绩条目的HTML列表
+    grades_entry = '<ul>'
     for _, row in student_grades.iterrows():
         # 检查成绩是否为数值类型
         if isinstance(row['百分成绩'], (int, float)):
@@ -10,17 +12,65 @@ def create_grades_entry(df, student_id):
         else:
             # 非数值类型的成绩直接转换为字符串
             grade = str(row['百分成绩'])
-        grades_entry.append(f"{row['课程名称']}: {grade}")
+        grades_entry += f"<li>{row['课程名称']}: {grade}</li>"
+    grades_entry += '</ul>'
     return grades_entry
 
+def generate_email_content(student_id, student_name, grade_entries):
+    # 将成绩条目转换为 HTML 格式的字符串
 
-def generate_email_content(student_name, grade_entries):
-    # 根据学生姓名和成绩条目生成邮件内容
-    grades_str = "\n".join(grade_entries)
-    email_content = f"亲爱的{student_name}同学:\n\n" \
-                    f"祝贺您顺利完成本学期的学习！教务处在此向您发送最新的成绩单。\n\n" \
-                    f"{grades_str}\n\n" \
-                    "希望您能够对自己的成绩感到满意，并继续保持努力和积极的学习态度。如果您在某些科目上没有达到预期的成绩，不要灰心，这也是学习过程中的一部分。我们鼓励您与您的任课教师或辅导员进行交流，他们将很乐意为您解答任何疑问并提供帮助。请记住，学习是一个持续不断的过程，我们相信您有能力克服困难并取得更大的进步。\n\n" \
-                    "再次恭喜您，祝您学习进步、事业成功！\n\n" \
-                    "教务处"
+
+    high_grades = []
+    low_grades = []
+
+    pattern = r"<li>(.*?)</li>"
+    matches = re.findall(pattern, grade_entries)
+    for entry in matches:
+        course,grade = entry.split(":")
+        grade = grade.strip()
+
+        if grade[0].isdigit():
+            grade = int(grade.split("分")[0])
+            if grade >= 90:
+                high_grades.append(course)
+            elif grade < 60:
+                low_grades.append(course)
+    high_grades_str = "，".join(high_grades)
+    low_grades_str = "，".join(low_grades)
+
+    grades_str = grade_entries
+    email_content = f"""
+    <html>
+    <head>
+        <style>
+            body {{ font-family: Arial, sans-serif; }}
+            li {{ margin-bottom: 5px; }}
+        </style>
+    </head>
+    <body>
+        <p>亲爱的<strong>{student_id}{student_name}</strong>同学:</p>
+        <p>祝贺您顺利完成本学期的学习！教务处在此向您发送最新的成绩单。</p>
+        <ul>
+            {grades_str}
+        </ul>
+    """
+
+    if high_grades_str:
+        email_content += f"<p>您在学科{high_grades_str}表现很好，请继续保持。</p>"
+
+    if low_grades_str:
+        email_content += f"<p>您在学科{low_grades_str}需要加油，请继续努力。</p>"
+
+    email_content += """
+        <p>希望您能够对自己的成绩感到满意，并继续保持努力和积极的学习态度。如果您在某些科目上没有达到预期的成绩，不要灰心，这也是学习过程中的一部分。我们鼓励您与您的任课教师或辅导员进行交流，他们将很乐意为您解答任何疑问并提供帮助。请记住，学习是一个持续不断的过程，我们相信您有能力克服困难并取得更大的进步。</p>
+        <p>再次恭喜您，祝您学习进步、事业成功！</p>
+        <p>教务处</p>
+    </body>
+    </html>
+    """
     return email_content
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 # 导入自定义的函数
 from read_excel import read_excel_file
 from email_content_generator import generate_email_content, create_grades_entry
+from send_email import send_email
+import tkinter as tk
+from tkinter import filedialog
 
+
+filepath = ""
+root = tk.Tk()
+root.geometry("400x200")
+root.title("成绩单发送程序")
+root.attributes('-alpha', 0.95)
 
 def main():
     # 定义Excel文件路径，假定文件位于项目的data文件夹下
-    excel_file_path = 'data/成绩表.xlsx'
+    # excel_file_path = 'data/成绩表.xlsx'
 
+    gui()
     # 读取Excel文件，获取DataFrame
-    df = read_excel_file(excel_file_path)
+    df = read_excel_file(filepath)
 
     # 确保DataFrame不为空
     if df is not None:
@@ -24,13 +34,37 @@ def main():
             student_name = df[df['学号'] == student_id]['姓名'].iloc[0]
 
             # 生成电子邮件内容
-            email_content = generate_email_content(student_name, grade_entries)
+            email_content = generate_email_content(student_id, student_name, grade_entries)
 
-            # 在控制台上打印邮件内容，用于测试或查看
-            print(email_content)
+            # print(email_content)
+            # 发送邮件
+            send_email(student_name, student_id, email_content)
 
-            # 打印分隔线以区分不同学生的邮件内容
-            print("------------------------------------------------\n")
+
+def end():
+    root.destroy()
+def gui():
+    file_frame = tk.Frame(root)
+    select_button = tk.Button(file_frame, text="选择文件", command=select_file, width=15, font=("Arial", 12, "bold"), bg="green", fg="white")
+    select_button.pack(side=tk.LEFT, padx=10)
+
+    global filepath_label
+    filepath_label = tk.Label(file_frame, text="未选择文件", bg="white", bd=1, relief=tk.SOLID, width=30, fg="black", font=("Arial", 12))
+    filepath_label.pack(side=tk.LEFT, padx=20)
+    file_frame.pack(pady=40)
+
+    send_button = tk.Button(root, text="发送文件", command=end, font=("Arial", 14, "bold"), bg="green", fg="white")
+    send_button.pack(pady=20)
+
+    root.configure(bg="white")
+    root.mainloop()
+
+def select_file():
+    global filepath_label  # 将filepath_label声明为全局变量
+    global filepath
+    filepath = filedialog.askopenfilename(initialdir="/", title="选择文件", filetypes=(("Excel文件", "*.xlsx"), ("所有文件", "*.*")))
+    if filepath:
+        filepath_label.config(text=filepath)
 
 
 # 确保当该脚本被直接运行时，会调用main函数
